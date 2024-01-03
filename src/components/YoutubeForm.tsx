b2dd973299d11fe8b5765f6093525ca62bef7b48
poring86 <matheuslino86a@gmail.com>
2024-01-03 17:25:04 -0400

    feat: :sparkles: Username required

--- src/components/YoutubeForm.tsx
+++ src/components/YoutubeForm.tsx
@@ -1,14 +1,27 @@
 import { useForm } from "react-hook-form";
 import { DevTool } from "@hookform/devtools"
 
+type FormValues = {
+  username: string
+  email: string
+  channel: string
+}
+
 export const YoutubeForm = () => {
-  const form = useForm()
-  const { register, control } = form
+  const form = useForm<FormValues>()
+
+  const { register, control, handleSubmit } = form
+
+  const onSubmit = (data: FormValues) => {
+    console.log('Form submitted', data)
+  }
 
   return <div>
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)} noValidate>
       <label className="label" htmlFor="username">Username</label>
-      <input className="input" type="text" id="username" {...register("username")}></input>
+      <input className="input" type="text" id="username" {...register("username", {
+        required: "Username is required"
+      })}></input>
 
       <label className="label" htmlFor="email">Email</label>
       <input className="input" type="email" id="email" {...register("email")}></input>
